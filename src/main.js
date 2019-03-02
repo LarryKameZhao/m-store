ac0f52db80d4a49008eee7c2703f888f6a3ff0bc
LarryZhao <larryzhao1994@outlook.com>
2019-03-02 20:11:30 +0800

    reset styles and symbols

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import 'normalize.css'
+import '@/assets/css/reset.css'
+import '@/assets/fonts/symbol.js'
 Vue.config.productionTip = false
 
 new Vue({
